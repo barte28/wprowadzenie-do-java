656600b9952b56ff7c4259e27628261a2933ba4b
barte28 <bartek.teuerle@gmail.com>
2024-09-04 13:40:22 +0200

    losowanie liczby rzeczywistej

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("na lekcji programowania");
+        System.out.println("Witaj ponownie");
+
+        double wylosowana = Math.random()*10+1;
+        System.out.println(wylosowana);
     }
 }
