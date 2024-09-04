76a693610cc88cdf833c4336901c18fac80b0411
barte28 <bartek.teuerle@gmail.com>
2024-09-04 13:42:19 +0200

    losowanie liczby calkowitej

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Witaj ponownie");
 
-        double wylosowana = Math.random()*10+1;
+        int wylosowana = (int)(Math.random()*10+1);
         System.out.println(wylosowana);
     }
 }
